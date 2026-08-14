03131553d8125987b80bc81792a154cefac386ff
Ararat <araratavetisyan777999@gmail.com>
2026-08-15 02:12:15 +0400

    Wire architecture modules

---
+++ kernel/src/arch/mod.rs
@@ -0,0 +1,8 @@
+#![no_std]
+
+#[cfg(target_arch = "x86_64")]
+pub mod x86_64;
+#[cfg(target_arch = "aarch64")]
+pub mod aarch64;
+#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
+pub mod riscv;
