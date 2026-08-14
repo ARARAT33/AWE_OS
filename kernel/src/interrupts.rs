9708adf43a83b022a7a0387c286dd67f3cf192a8
Ararat <araratavetisyan777999@gmail.com>
2026-08-15 02:14:01 +0400

    Add architecture-neutral interrupt handler registry

---
+++ kernel/src/interrupts.rs
@@ -0,0 +1,28 @@
+#![no_std]
+
+#[repr(C)]
+pub struct InterruptFrame {
+    pub rip: u64,
+    pub cs: u64,
+    pub rflags: u64,
+    pub rsp: u64,
+    pub ss: u64,
+}
+
+pub type Handler = extern "C" fn(&mut InterruptFrame);
+
+pub struct InterruptTable {
+    handlers: [Option<Handler>; 256],
+}
+
+impl InterruptTable {
+    pub const fn new() -> Self { Self { handlers: [None; 256] } }
+
+    pub fn register(&mut self, vector: u8, handler: Handler) {
+        self.handlers[vector as usize] = Some(handler);
+    }
+
+    pub fn get(&self, vector: u8) -> Option<Handler> {
+        self.handlers[vector as usize]
+    }
+}
