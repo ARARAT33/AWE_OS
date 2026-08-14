e1cbb724e794c4580712fe1706c2631410522c15
Ararat <araratavetisyan777999@gmail.com>
2026-08-15 02:11:03 +0400

    Add AArch64 early CPU control primitives

---
+++ kernel/src/arch/aarch64/boot.rs
@@ -0,0 +1,16 @@
+#![no_std]
+
+#[inline(always)]
+pub unsafe fn disable_interrupts() {
+    core::arch::asm!("msr daifset, #0xf", options(nomem, nostack, preserves_flags));
+}
+
+#[inline(always)]
+pub unsafe fn enable_interrupts() {
+    core::arch::asm!("msr daifclr, #0xf", options(nomem, nostack, preserves_flags));
+}
+
+#[inline(always)]
+pub unsafe fn wait() -> ! {
+    loop { core::arch::asm!("wfi", options(nomem, nostack, preserves_flags)); }
+}
