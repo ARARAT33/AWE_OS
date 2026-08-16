1f0532491d2412de81c6f4f3bbcaae1515088f68
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 15:39:21 +0400

    memory: expose paging primitives through kernel memory API

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -2,8 +2,10 @@
 
 pub mod allocator;
 pub mod frame;
+pub mod paging;
 pub mod region;
 
 pub use allocator::BumpAllocator;
 pub use frame::{frame_at, frame_end, Frame, PAGE_SIZE};
+pub use paging::{indices as page_indices, page_offset, PageFlags, PageTable, PageTableEntry};
 pub use region::{Region, RegionKind};
