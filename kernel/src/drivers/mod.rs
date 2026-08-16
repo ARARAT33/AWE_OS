36809398dfefeb21ff4e32a447f3cb7db29deb4c
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 18:29:00 +0400

    drivers: expose driver learning subsystem

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -3,9 +3,11 @@
 pub mod bus;
 pub mod compat;
 pub mod contract;
+pub mod learning;
 pub mod virtio;
 
 pub use bus::{DeviceId, DeviceKind, DriverBus};
 pub use compat::{bind_compatible_driver, validate_contract, CompatibilityRegistry, DriverManifest, DriverSource};
 pub use contract::{DeviceContract, DmaPolicy, InterruptMode, MmioRegion};
+pub use learning::{DriverExperience, ExperienceDb, ProbeOutcome};
 pub use virtio::{VirtioDevice, VirtioFeatures};
