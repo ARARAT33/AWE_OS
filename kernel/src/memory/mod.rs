dd7556a6e49370612c1a4d5c795aa1213a4106ed
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 18:08:53 +0400

    memory: expose typed physical and virtual address API

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -1,10 +1,12 @@
 #![no_std]
 
+pub mod address;
 pub mod allocator;
 pub mod frame;
 pub mod paging;
 pub mod region;
 
+pub use address::{PhysicalAddress, VirtualAddress};
 pub use allocator::BumpAllocator;
 pub use frame::{frame_at, frame_end, Frame, PAGE_SIZE};
 pub use paging::{indices as page_indices, page_offset, PageFlags, PageTable, PageTableEntry};
