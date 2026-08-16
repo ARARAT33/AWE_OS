9dab76adced37955361cdc998886fab6c59a58d5
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 20:50:18 +0400

    drivers: expose Linux Windows Android layers

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -1,19 +1,25 @@
 #![no_std]
 
+pub mod android;
 pub mod bus;
 pub mod compat;
 pub mod contract;
 pub mod core;
 pub mod learning;
+pub mod linux;
 pub mod virtio;
 pub mod universal;
 pub mod installer;
+pub mod windows;
 
+pub use android::AndroidLayer;
 pub use bus::{DeviceId, DeviceKind, DriverBus};
 pub use compat::{bind_compatible_driver, validate_contract, CompatibilityRegistry, DriverManifest, DriverSource};
 pub use contract::{DeviceContract, DmaPolicy, InterruptMode, MmioRegion};
 pub use core::{AdapterState, AndroidDriverAdapter, CoreError, DriverAdapter, DriverIdentity, DriverSlot, HardwareAbstraction, HardwareInfo, LinuxDriverAdapter, WindowsDriverAdapter};
 pub use learning::{DriverExperience, ExperienceDb, ProbeOutcome};
+pub use linux::LinuxLayer;
 pub use virtio::{VirtioDevice, VirtioFeatures};
 pub use universal::{validate_request, DriverAbi, DriverAction, DriverError, DriverOs, DriverRequest, DriverResult};
 pub use installer::{plan_install, InstallError, InstallPlan, InstallerPackage, PackageFormat};
+pub use windows::WindowsLayer;
