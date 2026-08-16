7a8d795829e6b0e787994129faba8fe0bafe1ecf
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 18:34:38 +0400

    kernel: expose syscall dispatch subsystem

---
+++ kernel/src/syscall/mod.rs
@@ -0,0 +1,7 @@
+#![no_std]
+
+pub mod abi;
+pub mod dispatch;
+
+pub use abi::{Syscall, SyscallResult, ERR_BUSY, ERR_INVALID_ARGUMENT, ERR_NO_MEMORY, ERR_NOT_FOUND, ERR_OK, ERR_PERMISSION};
+pub use dispatch::SyscallContext;
