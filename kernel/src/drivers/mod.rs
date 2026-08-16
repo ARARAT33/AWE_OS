9bb7c5fb6d2d153b2aa9b8c4cbad645b6bfdc0e0
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 20:49:55 +0400

    drivers: wire universal OS adapter core

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -3,6 +3,7 @@
 pub mod bus;
 pub mod compat;
 pub mod contract;
+pub mod core;
 pub mod learning;
 pub mod virtio;
 pub mod universal;
@@ -11,6 +12,7 @@ pub mod installer;
 pub use bus::{DeviceId, DeviceKind, DriverBus};
 pub use compat::{bind_compatible_driver, validate_contract, CompatibilityRegistry, DriverManifest, DriverSource};
 pub use contract::{DeviceContract, DmaPolicy, InterruptMode, MmioRegion};
+pub use core::{AdapterState, AndroidDriverAdapter, CoreError, DriverAdapter, DriverIdentity, DriverSlot, HardwareAbstraction, HardwareInfo, LinuxDriverAdapter, WindowsDriverAdapter};
 pub use learning::{DriverExperience, ExperienceDb, ProbeOutcome};
 pub use virtio::{VirtioDevice, VirtioFeatures};
 pub use universal::{validate_request, DriverAbi, DriverAction, DriverError, DriverOs, DriverRequest, DriverResult};
