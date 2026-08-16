e38013f7d71969cdda4bd762ff02e1c0fd1d6d01
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 20:47:22 +0400

    drivers: expose universal ABI and installer layers

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -5,9 +5,13 @@ pub mod compat;
 pub mod contract;
 pub mod learning;
 pub mod virtio;
+pub mod universal;
+pub mod installer;
 
 pub use bus::{DeviceId, DeviceKind, DriverBus};
 pub use compat::{bind_compatible_driver, validate_contract, CompatibilityRegistry, DriverManifest, DriverSource};
 pub use contract::{DeviceContract, DmaPolicy, InterruptMode, MmioRegion};
 pub use learning::{DriverExperience, ExperienceDb, ProbeOutcome};
 pub use virtio::{VirtioDevice, VirtioFeatures};
+pub use universal::{validate_request, DriverAbi, DriverAction, DriverError, DriverOs, DriverRequest, DriverResult};
+pub use installer::{plan_install, InstallError, InstallPlan, InstallerPackage, PackageFormat};
