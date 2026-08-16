70a2f7f887bc14537d4580762864cf3b9d734b10
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 15:03:56 +0400

    kernel: correct memory module exports

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -5,5 +5,5 @@ pub mod frame;
 pub mod region;
 
 pub use allocator::BumpAllocator;
-pub use frame::Frame;
-pub use region::{MemoryRegion, MemoryRegionKind};
+pub use frame::{frame_at, frame_end, Frame, PAGE_SIZE};
+pub use region::{Region, RegionKind};
