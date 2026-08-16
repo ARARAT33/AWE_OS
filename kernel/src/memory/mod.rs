f95d1490050552da66903862ca5c8ab72978be3f
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 18:41:59 +0400

    memory: expose deterministic page mapper

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -3,11 +3,13 @@
 pub mod address;
 pub mod allocator;
 pub mod frame;
+pub mod mapper;
 pub mod paging;
 pub mod region;
 
 pub use address::{PhysicalAddress, VirtualAddress};
 pub use allocator::BumpAllocator;
 pub use frame::{frame_at, frame_end, Frame, PAGE_SIZE};
-pub use paging::{indices as page_indices, page_offset, PageFlags, PageTable, PageTableEntry};
+pub use mapper::{MapError, PageMapper};
+pub use paging::{indices as page_indices, page_offset, valid_mapping, PageFlags, PageTable, PageTableEntry};
 pub use region::{Region, RegionKind};
