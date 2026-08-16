5b52ec4e27951fd4faca422e1e884afd38f62d09
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 20:43:53 +0400

    memory: expose physical bitmap allocator

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -6,12 +6,14 @@ pub mod boot_mapper;
 pub mod frame;
 pub mod mapper;
 pub mod paging;
+pub mod physical;
 pub mod region;
 
 pub use address::{PhysicalAddress, VirtualAddress};
 pub use allocator::BumpAllocator;
 pub use boot_mapper::{BootMapError, BootMapper};
-pub use frame::{frame_at, frame_end, Frame, PAGE_SIZE};
+pub use frame::{frame_at, frame_end, Frame, PhysicalFrameAllocator, PAGE_SIZE};
 pub use mapper::{MapError, PageMapper};
 pub use paging::{indices as page_indices, page_offset, valid_mapping, PageFlags, PageTable, PageTableEntry};
+pub use physical::{FrameBitmap, PhysicalMemoryError};
 pub use region::{Region, RegionKind};
