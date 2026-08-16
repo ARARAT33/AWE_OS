c069e044e97f79e1d88543700ca95bad9e673cc0
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 18:21:53 +0400

    drivers: expose cross-OS compatibility layer

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -1,9 +1,11 @@
 #![no_std]
 
 pub mod bus;
+pub mod compat;
 pub mod contract;
 pub mod virtio;
 
 pub use bus::{DeviceId, DeviceKind, DriverBus};
+pub use compat::{bind_compatible_driver, validate_contract, CompatibilityRegistry, DriverManifest, DriverSource};
 pub use contract::{DeviceContract, DmaPolicy, InterruptMode, MmioRegion};
 pub use virtio::{VirtioDevice, VirtioFeatures};
