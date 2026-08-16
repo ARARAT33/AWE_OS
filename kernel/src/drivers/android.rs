c90854f7596ce11abfd9a1e1b5e560d87c30e4e4
Ararat <araratavetisyan777999@gmail.com>
2026-08-16 23:03:48 +0400

    drivers: add Android HAL adapter validation tests

--- kernel/src/drivers/android.rs
+++ kernel/src/drivers/android.rs
@@ -16,3 +16,17 @@ impl<A:AndroidDriverAdapter> AndroidLayer<A>{
   Ok(())
  }
 }
+
+#[cfg(test)]
+mod tests{
+ use super::*; use super::super::bus::DeviceId;
+ struct Mock{api:u32,name:&'static str,os:DriverOs,abi:DriverAbi,vendor:u16,device:u16,signed:bool}
+ impl DriverAdapter for Mock{fn identity(&self)->super::super::core::DriverIdentity{super::super::core::DriverIdentity{os:self.os,abi:self.abi,api_version:self.api as u16,vendor:self.vendor,device:self.device,signed:self.signed}}fn probe(&mut self,_:&HardwareInfo)->Result<(),CoreError>{Ok(())}fn start(&mut self,_:&HardwareInfo)->Result<(),CoreError>{Ok(())}fn stop(&mut self,_:&HardwareInfo)->Result<(),CoreError>{Ok(())}fn remove(&mut self,_:&HardwareInfo)->Result<(),CoreError>{Ok(())}}
+ impl AndroidDriverAdapter for Mock{fn android_hal_version(&self)->u32{self.api}fn android_interface_name(&self)->&'static str{self.name}}
+ fn hw()->HardwareInfo{HardwareInfo{id:DeviceId{vendor:1,device:2,class:0,revision:1},mmio_base:0x1000,mmio_length:0x100,irq:5,dma_bits:64}}
+ fn good()->AndroidLayer<Mock>{AndroidLayer::new(Mock{api:7,name:"awe-hal",os:DriverOs::Android,abi:DriverAbi::AndroidHal,vendor:1,device:2,signed:true})}
+ #[test]fn valid_adapter_passes(){assert!(good().validate(&hw()).is_ok())}
+ #[test]fn mismatched_identity_is_rejected(){let mut x=good();x.slot.adapter.device=9;assert_eq!(x.validate(&hw()),Err(CoreError::InvalidDevice))}
+ #[test]fn api_mismatch_is_rejected(){let mut x=good();x.slot.adapter.api=8;assert_eq!(x.validate(&hw()),Err(CoreError::InvalidRequest))}
+ #[test]fn unsigned_driver_is_rejected(){let mut x=good();x.slot.adapter.signed=false;assert_eq!(x.validate(&hw()),Err(CoreError::InvalidDevice))}
+}
