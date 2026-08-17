aca9db29964adc734a3d296a3b081e7738b5b49d
Ararat <araratavetisyan777999@gmail.com>
2026-08-17 14:09:06 +0400

    AWE_OS 65%: export complete platform discovery stack

--- services/driverd/src/lib.rs
+++ services/driverd/src/lib.rs
@@ -6,11 +6,13 @@
 
 mod access;
 mod acpi;
+mod acpi_root;
 mod apic;
 mod catalog;
 mod contract;
 mod dependency;
 mod pci;
+mod pci_x86;
 mod protocol;
 mod reference;
 mod registry;
@@ -19,11 +21,14 @@ mod virtio;
 
 pub use access::{AccessKind, AccessRegion, HardwareAccessPlan, InterruptMode, InterruptOwnership, PowerState, power_transition_allowed};
 pub use acpi::{AcpiError, AcpiTableRef, MadtRecord, SdtHeader, checksum_ok, find_table, parse_header, parse_madt};
+pub use acpi_root::{RsdpError, RsdpInfo, parse_pointer_table, validate_rsdp};
 pub use apic::{ApicError, IoApic, IrqRoute, LocalApic};
 pub use catalog::{BuiltinDriver, BUILTIN_DRIVER_COUNT, BUILTIN_DRIVERS, descriptors};
 pub use contract::{DriverLifecycle, DriverManifest, DriverTrust, LifecycleError, lifecycle_maps_to_state, transition};
 pub use dependency::{DependencyError, DependencyGraph, DriverHealth, DriverDependency, ResourceOwnership};
 pub use pci::{PciConfigAccess, PciDevice, PciDeviceTable, PciError, PciLocation, enumerate as enumerate_pci};
+#[cfg(target_arch = "x86_64")]
+pub use pci_x86::X86CfgIo;
 pub use protocol::{DriverClass, DriverCommand, DriverEvent, DriverId, DriverReply, DriverState};
 pub use reference::{BlockRequest, DisplayRect, InputEvent, NetFrame, ReferenceDevice, ReferenceError, validate_block_request, validate_display_rect};
 pub use registry::{DriverDescriptor, DriverRegistry, RegistryError};
