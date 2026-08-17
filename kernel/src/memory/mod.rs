208f44f6c6d0f2bae3c62403905ecaba1f41a0bd
Ararat <araratavetisyan777999@gmail.com>
2026-08-17 13:23:48 +0400

    kernel: expose x86_64 bootstrap paging module

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -8,6 +8,8 @@ pub mod mapper;
 pub mod paging;
 pub mod physical;
 pub mod region;
+#[cfg(target_arch = "x86_64")]
+pub mod x86_64;
 
 pub use address::{PhysicalAddress, VirtualAddress};
 pub use allocator::BumpAllocator;
@@ -16,4 +18,6 @@ pub use frame::{frame_at, frame_end, Frame, PhysicalFrameAllocator, PAGE_SIZE};
 pub use mapper::{MapError, PageMapper};
 pub use paging::{indices as page_indices, page_offset, valid_mapping, PageFlags, PageTable, PageTableEntry};
 pub use physical::{FrameBitmap, PhysicalMemoryError};
+#[cfg(target_arch = "x86_64")]
+pub use x86_64::activate_bootstrap_identity_map;
 pub use region::{Region, RegionKind};
